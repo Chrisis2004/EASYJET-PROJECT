66f96687aa99468462f0f368576468b83124b186
Chrisis2004 <christian.canossa04@gmail.com>
2022-02-10 20:58:59 +0100

    FileIO change

--- src/IO/FileIO.java
+++ src/IO/FileIO.java
@@ -1,4 +1,5 @@
 package IO;
+
 import java.io.File;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -6,40 +7,42 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.LineNumberReader;
+
 public class FileIO {
     private File file;
     public int nLines;
 
-    public FileIO(String fileName) throws IOException
-    {
+    public FileIO(String fileName) throws IOException {
         this.file = new File(fileName);
         this.nLines = findLineAmount();
     }
+
     public int findLineAmount() throws IOException {
         LineNumberReader lineNumberReader = new LineNumberReader(new FileReader(file));
         lineNumberReader.skip(Long.MAX_VALUE);
         int totalLines = lineNumberReader.getLineNumber();
         lineNumberReader.close();
         return totalLines;
-    } 
-    public String[] readFromFile() throws IOException{
+    }
+
+    public String[] readFromFile() throws IOException {
         int nLines = findLineAmount();
-        if (nLines!=0) {
+        if (nLines != 0) {
             BufferedReader fileReader = new BufferedReader(new FileReader(this.file));
-            String[] linesFromFile= new String[nLines];
-            for(int i=0; i<nLines;i++)
-                linesFromFile[i]=fileReader.readLine();
+            String[] linesFromFile = new String[nLines];
+            for (int i = 0; i < nLines; i++)
+                linesFromFile[i] = fileReader.readLine();
             fileReader.close();
             return linesFromFile;
-        }
-        else {
+        } else {
             System.out.println("The file is void");
             return null;
-        } 
+        }
     }
+
     public void writeOnFile(String[] toWrite) throws IOException {
         BufferedWriter fileWriter = new BufferedWriter(new FileWriter(this.file));
-        for (int i=0;i<toWrite.length;i++){
+        for (int i = 0; i < toWrite.length; i++) {
             fileWriter.write(toWrite[i]);
             fileWriter.write("\n");
         }
@@ -47,7 +50,7 @@ public class FileIO {
         fileWriter.close();
     }
 
-    public int howManySplit(String separatorChar) throws IOException{
+    public int howManySplit(String separatorChar) throws IOException {
         BufferedReader fileReader = new BufferedReader(new FileReader(this.file));
         String[] inLines = fileReader.readLine().split(separatorChar);
         int splitNumbers = inLines.length;
@@ -55,30 +58,28 @@ public class FileIO {
         return splitNumbers;
     }
 
-    public String[][] readFromCSV(String separatorChar) throws IOException{
+    public String[][] readFromCSV(String separatorChar) throws IOException {
         BufferedReader fileReader = new BufferedReader(new FileReader(this.file));
         int split = howManySplit(separatorChar);
         int nLines = findLineAmount();
         String[][] result = new String[nLines][split];
-        if (nLines!=0) {
-            for(int z=0; z<nLines;z++){
+        if (nLines != 0) {
+            for (int z = 0; z < nLines; z++) {
                 String[] inLines = fileReader.readLine().split(separatorChar);
-                for (int i=0;i<split;i++) {
+                for (int i = 0; i < split; i++) {
                     result[z][i] = inLines[i];
                 }
             }
-        } 
-        else
+        } else
             result = null;
         fileReader.close();
         return result;
     }
-    
-    public void writeOnCSV(String[][] toWrite, int nLines, int nSplits, String separatorChar) throws IOException
-    {
+
+    public void writeOnCSV(String[][] toWrite, int nLines, int nSplits, String separatorChar) throws IOException {
         BufferedWriter fileWriter = new BufferedWriter(new FileWriter(this.file));
-        for (int i=0;i<nLines;i++){
-            for(int z=0;z<nSplits;z++)
+        for (int i = 0; i < nLines; i++) {
+            for (int z = 0; z < nSplits; z++)
                 fileWriter.write(toWrite[i][z] + separatorChar);
             fileWriter.write("\n");
         }
@@ -86,77 +87,81 @@ public class FileIO {
         fileWriter.close();
     }
 
-    public void deleteFile(){
+    public void deleteFile() {
         this.file.delete();
     }
 
-    public void writeOnCSVAddOnly(String[][] toWrite, int nLines, int nSplits, String separatorChar) throws IOException
-    {
+    public void writeOnCSVAddOnly(String[][] toWrite, int nLines, int nSplits, String separatorChar)
+            throws IOException {
         String[] fileContent = readFromFile();
         int nLinesFile = findLineAmount();
         BufferedWriter fileWriter = new BufferedWriter(new FileWriter(this.file));
-        for (int i=0;i<nLinesFile;i++){
+        for (int i = 0; i < nLinesFile; i++) {
             fileWriter.write(fileContent[i]);
             fileWriter.write("\n");
         }
-        for (int i=0;i<nLines;i++)
-            for(int z=0;z<nSplits;z++)
+        for (int i = 0; i < nLines; i++)
+            for (int z = 0; z < nSplits; z++)
                 fileWriter.write(toWrite[i][z] + separatorChar);
         fileWriter.flush();
         fileWriter.close();
     }
 
-    public int thereIsOnCSV(String toSearch, int splitToSearch) throws IOException{
+    public int thereIsOnCSV(String toSearch, int splitToSearch) throws IOException {
         String[][] fileContent = readFromCSV(";");
-        for (int i=0;i<findLineAmount();i++){
-            if(fileContent[i][splitToSearch].equals(toSearch))
+        for (int i = 0; i < findLineAmount(); i++) {
+            if (fileContent[i][splitToSearch].equals(toSearch))
                 return i;
         }
         return -1;
     }
-    public boolean thereIsInLineCSV(String toSearch, int nLine, int splitToSearch) throws IOException
-    {
+
+    public boolean thereIsInLineCSV(String toSearch, int nLine, int splitToSearch) throws IOException {
         String[][] fileContent = readFromCSV(";");
         return fileContent[nLine][splitToSearch].equals(toSearch);
     }
-    public String getFromCSV (int nLine, int nSplit) throws IOException{
+
+    public String getFromCSV(int nLine, int nSplit) throws IOException {
         String[][] fileContent = readFromCSV(";");
         String toReturn = fileContent[nLine][nSplit];
-            return toReturn;
+        return toReturn;
     }
-    public void delateLineFromCSV(int nLineToDelate) throws IOException{
+
+    public void delateLineFromCSV(int nLineToDelate) throws IOException {
         String[] fileContent = readFromFile();
         int nLines = findLineAmount();
         BufferedWriter fileWriter = new BufferedWriter(new FileWriter(this.file));
-        for (int i=0;i<nLines;i++){
-            if (i==nLineToDelate)
+        for (int i = 0; i < nLines; i++) {
+            if (i == nLineToDelate)
                 continue;
             fileWriter.write(fileContent[i]);
             fileWriter.write("\n");
         }
         fileWriter.close();
     }
+
     public void writeOnFileAddOnly(String[] toWrite) throws IOException {
         String[] fileContent = readFromFile();
         int nLines = findLineAmount();
         BufferedWriter fileWriter = new BufferedWriter(new FileWriter(this.file));
-        for (int i=0;i<nLines;i++){
+        for (int i = 0; i < nLines; i++) {
             fileWriter.write(fileContent[i]);
             fileWriter.write("\n");
         }
-        for (int i=0;i<toWrite.length;i++){
+        for (int i = 0; i < toWrite.length; i++) {
             fileWriter.write(toWrite[i]);
             fileWriter.write("\n");
-        }       
+        }
         fileWriter.flush();
         fileWriter.close();
     }
-    public void delateLineFromFile(int nLineToDelate) throws IOException{
+
+    public void delateLineFromFile(int nLineToDelate) throws IOException {
         String[] fileContent = readFromFile();
         int nLines = findLineAmount();
         BufferedWriter fileWriter = new BufferedWriter(new FileWriter(this.file));
-        for (int i=0;i<nLines;i++){
-            if (i==nLineToDelate)
+        for (int i = 0; i < nLines; i++) {
+            if (i == nLineToDelate)
                 continue;
             fileWriter.write(fileContent[i]);
             fileWriter.write("\n");
